2de798e24618b5ead3eb3c53f508f84d53aba72a
Desmond <desmond.soon@amway.com>
2022-07-13 15:26:04 +0800

    Initial commit

--- infra/config/preprod/updateLabelPrintingAPICICDStack.sh
+++ infra/config/preprod/updateLabelPrintingAPICICDStack.sh
@@ -1,3 +1,3 @@
 # Author: Desmond.Soon@Amway.com
-# Project: CMP to Zendesk Incidents Migration
+# Project: Label Printing API
 aws cloudformation update-stack --stack-name ApacMsbPreprodLabelPrintingAPI --template-body file://LabelPrintingAPICICD.yml --parameters file://LabelPrintingAPICICD-params.json --region ap-southeast-1 --tags file://LabelPrintingAPICICD-tags.json --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
