9a7e34303ccceffa784ba22af0ff48900f7ed5bc
Desmond <desmond.soon@amway.com>
2022-07-13 14:50:27 +0800

    Initial Commit

---
+++ LabelPrintingApi.py
@@ -0,0 +1,13 @@
+import json
+import Constants
+import logging
+import os
+import boto3
+from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch, RequestsHttpConnection, exceptions, helpers
+from datetime import datetime
+
+
+
+def lambda_handler(event, context):
+    return
